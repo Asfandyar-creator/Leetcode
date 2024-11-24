9e44895176b31c1433fdbc3c203ece4a0c7f5d34
Asfandyar <yar69416@gmail.com>
2024-11-24 01:04:58 -0800

    Refactor Dog and Circle classes; add validation and area calculation methods

--- Arrays/contains_duplicate_easy.py
+++ Arrays/contains_duplicate_easy.py
@@ -1,28 +1,53 @@
-class Car:
-    def __init__(self, color, mileage):
-        self.color = color
-        self.mileage = mileage
-    def __str__(self):
-        return f'a {self.color} car with {self.mileage} miles'
+"""
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-blue_car = Car('blue', 20)
-print(blue_car)
+ 
 
-class Parent:
-    speaks = ['English']
+Example 1:
 
-class Child(Parent):
-    def __init___(self):
-        super().__init__()
-        self.speaks = ['Spanish']
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+"""
 
 
-class Dog:
-    def __init__(self, name, age, breed):
-        self.name = name
-        self.age = age
-        self.breed = breed
-    def __str__(self):
-        return f'{self.name} is a {self.age} year old {self.breed}'
-    def __repr__(self):
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        num_set = set(nums)
+        if len(nums) != len(num_set):
+            return True
+        else:
+            return False
         
